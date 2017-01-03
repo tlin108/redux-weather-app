e6107dd123e8686016eace3428cebd756194aecf
tlin108 <tlin108@hotmail.com>
2017-01-02 21:51:12 -0500

    remake into chart component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const pressures = cityData.list.map(weather => weather.main.pressre);
 
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Chart data={temps} color="orange" />
+        </td>
       </tr>
     );
   }
+
   render() {
     return (
       <table className="table table-hover">
