16598c7abe991ef72f8d8a86edab2e1b3b4803e9
tlin108 <tlin108@hotmail.com>
2017-01-14 21:58:16 -0500

    edit http request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'd22aa35f1d715ba35838639d67d4cff8';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
